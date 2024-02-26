2d71998f0be0696864ae7213b8f03d9b47e2c7f3
Your Name <gabrielorbeta11@gmail.com>
2024-02-26 17:51:00 +0800

    first commit

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+// An array of public routes that don't require authentication.
+publicRoutes: ["/api/webhook/clerk"],
+
+// An array of routes to be ignored by the authentication middleware.
+ignoredRoutes: ["/api/webhook/clerk"],
+});
+ 
+export const config = {
+  // Protects all routes, including api/trpc.
+  // See https://clerk.com/docs/references/nextjs/auth-middleware
+  // for more information about configuring your Middleware
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
